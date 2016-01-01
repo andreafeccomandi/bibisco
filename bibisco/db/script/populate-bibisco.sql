ff5bc2a567b09620aad67c1c794720731a325fc8
Andrea Feccomandi <andreafeccomandi@gmail.com>
2016-01-01 16:50:04 +0100

    Removed projectsDirectory from properties

--- bibisco/db/script/populate-bibisco.sql
+++ bibisco/db/script/populate-bibisco.sql
@@ -10,5 +10,4 @@ insert into properties (property,value) values ('charactersdndTip','true');
 insert into properties (property,value) values ('strandsdndTip','true');
 insert into properties (property,value) values ('donationTip','');
 insert into properties (property,value) values ('socialMediaTip','true');
-insert into properties (property,value) values ('projectsDirectory','');
 insert into properties (property,value) values ('locale','');
