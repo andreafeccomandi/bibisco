538a4c279e8fe33092ea43fd92baf40f99e37fe8
Andrea Feccomandi <andrea.feccomandi@gmail.com>
2016-09-12 19:13:39 +0200

    Upgrade to version 1.4.1

--- bibisco/db/script/populate-bibisco.sql
+++ bibisco/db/script/populate-bibisco.sql
@@ -3,7 +3,7 @@ insert into properties (property,value) values ('font-size','medium');
 insert into properties (property,value) values ('indentParagraphEnabled','true');
 insert into properties (property,value) values ('spellCheckEnabled','true');
 insert into properties (property,value) values ('autoSaveEnabled','true');
-insert into properties (property,value) values ('version','1.4.0');
+insert into properties (property,value) values ('version','1.4.1');
 insert into properties (property,value) values ('sceneTip','true');
 insert into properties (property,value) values ('chaptersdndTip','true');
 insert into properties (property,value) values ('scenesdndTip','true');
