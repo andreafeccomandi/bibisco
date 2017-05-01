15bcddc3a148689378baceddda475906a123b5c1
Andrea Feccomandi <andreafeccomandi@gmail.com>
2017-05-02 00:29:36 +0200

    Added language-input-field

---
+++ bibisco/app/components/forms/language-input-field/language-input-field.js
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2014-2017 Andrea Feccomandi
+ *
+ * Licensed under the terms of GNU GPL License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ * See the GNU General Public License for more details.
+ *
+ */
+angular.
+module('bibiscoApp').
+component('languageinputfield', {
+  templateUrl: 'components/forms/language-input-field/language-input-field.html',
+  controller: LanguageInputFieldController,
+  bindings: {
+    applyonchange: '<',
+    inputcols: '@',
+    label: '@',
+    labelcols: '@',
+    onselectlanguage: '&'
+  }
+});
+
+function LanguageInputFieldController(LocaleService, LoggerService) {
+  LoggerService.debug('Start LanguageInputFieldController...');
+
+  var self = this;
+
+  LoggerService.debug('End LanguageInputFieldController...');
+}
