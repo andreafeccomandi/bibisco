0f623272914e47c22347031d61517611ca08ed7b
Andrea Feccomandi <andreafeccomandi@gmail.com>
2017-05-12 22:30:04 +0200

    Added words character counter

---
+++ bibisco/app/components/typography/words-characters-counter/words-characters-counter.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2014-2017 Andrea Feccomandi
+ *
+ * Licensed under the terms of GNU GPL License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ * See the GNU General Public License for more details.
+ *
+ */
+angular.
+module('bibiscoApp').
+component('wordscharacterscounter', {
+  templateUrl: 'components/typography/words-characters-counter/words-characters-counter.html',
+  controller: WordsCharactersCounter,
+  bindings: {
+    characters: '<',
+    words: '<'
+  }
+});
+
+
+function WordsCharactersCounter(LoggerService) {
+
+  LoggerService.debug('Start WordsCharactersCounter...');
+
+  var self = this;
+
+  LoggerService.debug('End WordsCharactersCounter...');
+}
