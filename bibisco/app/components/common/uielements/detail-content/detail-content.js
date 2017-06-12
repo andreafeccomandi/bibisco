4fba4dc2c9f7a9cc80a5f075c7ae414517f42d29
Andrea Feccomandi <andreafeccomandi@gmail.com>
2017-06-13 00:27:30 +0200

    Detail content first implementation

---
+++ bibisco/app/components/common/uielements/detail-content/detail-content.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2014-2017 Andrea Feccomandi
+ *
+ * Licensed under the terms of GNU GPL License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ * See the GNU General Public License for more details.
+ *
+ */
+angular.
+module('bibiscoApp').
+component('detailcontent', {
+  templateUrl: 'components/common/uielements/detail-content/detail-content.html',
+  controller: DetailContentController,
+  bindings: {
+    content: '@',
+    contentstyle: '@',
+    mode: '<',
+    subtitle: '@'
+  }
+});
+
+
+function DetailContentController(LoggerService) {
+
+  LoggerService.debug('Start DetailContentController...');
+
+  var self = this;
+
+  LoggerService.debug('End DetailContentController...');
+}
---
+++ bibisco/app/components/common/uielements/detail-content/detail-view-content.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2014-2017 Andrea Feccomandi
+ *
+ * Licensed under the terms of GNU GPL License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ * See the GNU General Public License for more details.
+ *
+ */
+angular.
+module('bibiscoApp').
+component('detailviewcontent', {
+  templateUrl: 'components/common/uielements/detail-content/detail-view-content.html',
+  controller: DetailViewContentController,
+  bindings: {
+    content: '@',
+    contentstyle: '@'
+  }
+});
+
+
+function DetailViewContentController(LoggerService) {
+
+  LoggerService.debug('Start DetailViewContentController...');
+
+  var self = this;
+
+  LoggerService.debug('End DetailViewContentController...');
+}
