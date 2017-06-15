6b2af224fd7113a4c512392cf2c675e0f1e69c1f
Andrea Feccomandi <andreafeccomandi@gmail.com>
2017-06-15 08:50:42 +0200

    Added button with confirm

---
+++ bibisco/app/components/common/forms/button-with-confirm/button-with-confirm.js
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2014-2017 Andrea Feccomandi
+ *
+ * Licensed under the terms of GNU GPL License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ * See the GNU General Public License for more details.
+ *
+ */
+angular.
+module('bibiscoApp').
+component('buttonwithconfirm', {
+  templateUrl: 'components/common/forms/button-with-confirm/button-with-confirm.html',
+  controller: ButtonWithConfirmController,
+  bindings: {
+    buttonfunction: '&',
+    buttonlabel: '@',
+    buttonstyle: '@',
+    confirmmessage: '@',
+    enableconfirm: '<'
+  }
+});
+
+
+function ButtonWithConfirmController($uibModal, LoggerService) {
+
+  LoggerService.debug('Start ButtonWithConfirmController...');
+
+  var self = this;
+
+  self.click = function() {
+    if (self.enableconfirm) {
+      self.openComponentModal();
+    } else {
+      self.buttonfunction();
+    }
+  }
+
+  self.openComponentModal = function() {
+    var modalInstance = $uibModal.open({
+      animation: true,
+      component: 'modalconfirm',
+      resolve: {
+        message: function() {
+          return self.confirmmessage;
+        }
+      }
+    });
+
+    modalInstance.result.then(function(selectedItem) {
+      self.buttonfunction();
+    }, function() {
+      // cancel
+    });
+  };
+
+  LoggerService.debug('End ButtonWithConfirmController...');
+}
