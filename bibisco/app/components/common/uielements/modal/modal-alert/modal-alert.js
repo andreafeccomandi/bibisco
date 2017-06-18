cebee66fcd85d636d20ba64003c861b44bba1954
Andrea Feccomandi <andreafeccomandi@gmail.com>
2017-06-18 11:56:57 +0200

    Added modal-alert

---
+++ bibisco/app/components/common/uielements/modal/modal-alert/modal-alert.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2014-2017 Andrea Feccomandi
+ *
+ * Licensed under the terms of GNU GPL License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ * See the GNU General Public License for more details.
+ *
+ */
+angular.
+module('bibiscoApp').
+component('modalalert', {
+  templateUrl: 'components/common/uielements/modal/modal-alert/modal-alert.html',
+  controller: ModalAlertController,
+  bindings: {
+    dismiss: '&',
+    resolve: '<'
+  },
+});
+
+function ModalAlertController(LoggerService) {
+  LoggerService.debug('Start ModalAlertController...');
+
+  var self = this;
+
+  LoggerService.debug('End ModalAlertController...');
+}
