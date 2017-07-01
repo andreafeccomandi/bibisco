1098e73b16e0b7e0d3ee0192b283821a0dec1153
Andrea Feccomandi <andreafeccomandi@gmail.com>
2017-07-01 20:22:12 +0200

    First implementation of rich text editor

---
+++ bibisco/app/components/common/uielements/richtexteditor/richtexteditor.js
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2014-2017 Andrea Feccomandi
+ *
+ * Licensed under the terms of GNU GPL License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ * See the GNU General Public License for more details.
+ *
+ */
+angular.
+module('bibiscoApp').
+component('richtexteditor', {
+  templateUrl: 'components/common/uielements/richtexteditor/richtexteditor.html',
+  controller: RichTextEditorController,
+  bindings: {
+    content: '@',
+    contentstyle: '@'
+  }
+});
+
+
+function RichTextEditorController($document, LoggerService) {
+
+  LoggerService.debug('Start RichTextEditorController...');
+
+  var self = this;
+  self.editor = null;
+
+  self.editorCreated = function(editor) {
+    console.log('Editor creato!');
+    self.editor = editor;
+  }
+
+  self.undo = function() {
+    self.editor.history.undo();
+  }
+
+  self.redo = function() {
+    self.editor.history.redo();
+  }
+
+  self.copy = function() {
+    $document[0].execCommand('copy');
+  }
+
+  self.cut = function() {
+    $document[0].execCommand('cut');
+  }
+
+  self.paste = function() {
+    $document[0].execCommand('paste');
+  }
+
+  self.leftguillemet = function() {
+    $document[0].execCommand('insertText', false, '«');
+  }
+
+  self.rightguillemet = function() {
+    $document[0].execCommand('insertText', false, '»');
+  }
+
+  self.emdash = function() {
+    $document[0].execCommand('insertText', false, '—');
+  }
+
+  LoggerService.debug('End RichTextEditorController...');
+}
