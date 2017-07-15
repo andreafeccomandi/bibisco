62e7b31f34370668453642bb5f7448ea95db6d2d
Andrea Feccomandi <andreafeccomandi@gmail.com>
2017-07-15 17:49:14 +0200

    First implementation of spellchecker

--- bibisco/app/components/common/uielements/richtexteditor/richtexteditor.js
+++ bibisco/app/components/common/uielements/richtexteditor/richtexteditor.js
@@ -29,6 +29,28 @@ function RichTextEditorController($document, $scope, $timeout, hotkeys,
 
   LoggerService.debug('Start RichTextEditorController...');
 
+  const electronSpellchecker = require('electron-spellchecker');
+
+  // Retrieve required properties
+  const SpellCheckHandler = electronSpellchecker.SpellCheckHandler;
+  const ContextMenuListener = electronSpellchecker.ContextMenuListener;
+  const ContextMenuBuilder = electronSpellchecker.ContextMenuBuilder;
+
+  // Configure the spellcheckhandler
+  window.spellCheckHandler = new SpellCheckHandler();
+  window.spellCheckHandler.attachToInput();
+
+  // Start off as "US English, America"
+  window.spellCheckHandler.switchLanguage('en-US');
+
+  // Create the builder with the configured spellhandler
+  let contextMenuBuilder = new ContextMenuBuilder(window.spellCheckHandler);
+
+  // Add context menu listener
+  let contextMenuListener = new ContextMenuListener((info) => {
+    contextMenuBuilder.showPopupMenu(info);
+  });
+
   var self = this;
 
   if (ContextService.getOs() == 'darwin') {
