ab78ea7dae82e63611a3ba61c48d4beb682ffe86
Andrea Feccomandi <andreafeccomandi@gmail.com>
2017-09-08 07:48:07 +0200

    Added architecture service

---
+++ bibisco/app/services/ArchitectureService.js
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2014-2017 Andrea Feccomandi
+ *
+ * Licensed under the terms of GNU GPL License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ * See the GNU General Public License for more details.
+ *
+ */
+
+angular.module('bibiscoApp').service('ArchitectureService', function(
+  CollectionUtilService, LoggerService, ProjectDbConnectionService
+) {
+  'use strict';
+
+  var collection = ProjectDbConnectionService.getProjectDb().getCollection(
+    'architecture');
+
+  return {
+    getFabula: function(id) {
+      return collection.findOne({
+        type: 'fabula'
+      });
+    },
+    getPremise: function(id) {
+      return collection.findOne({
+        type: 'premise'
+      });
+    },
+    getSetting: function() {
+      return collection.findOne({
+        type: 'setting'
+      });
+    },
+    update: function(architectureItem) {
+      CollectionUtilService.update(collection, architectureItem);
+    }
+  }
+});
