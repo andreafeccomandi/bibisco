523c8051e491c3a17a3584bddb32bdcfc77e777e
Andrea Feccomandi <andreafeccomandi@gmail.com>
2017-09-15 08:49:55 +0200

    Managed title mandatory on element-title-form

--- bibisco/app/components/common/uielements/element-title-form/element-title-form.js
+++ bibisco/app/components/common/uielements/element-title-form/element-title-form.js
@@ -24,6 +24,7 @@ component('elementtitleform', {
     pageheadertitle: '@',
     savefunction: '&',
     titlelabel: '@',
+    titlemandatory: '<',
     titlemaxlength: '@',
     titlevalue: '@'
   }
