4c6f545105e12c474137be489bd5bb23025f5f7b
Andrea Feccomandi <andreafeccomandi@gmail.com>
2017-10-11 08:42:33 +0200

    Scene tags: added povs, characters, locations, strands

--- bibisco/app/components/chapters/scene-tags.js
+++ bibisco/app/components/chapters/scene-tags.js
@@ -20,7 +20,8 @@ component('scenetags', {
 });
 
 function SceneTitleController($location, $routeParams, ChapterService,
-  LoggerService) {
+  LocationService, LoggerService, MainCharacterService,
+  SecondaryCharacterService, StrandService) {
   LoggerService.debug('Start SceneTitleController...');
 
   var self = this;
@@ -45,6 +46,17 @@ function SceneTitleController($location, $routeParams, ChapterService,
       label: 'jsp.scene.title.tags'
     });
 
+    // init point of views
+    self.initPointOfViews();
+
+    // init characters
+    self.initCharacters();
+
+    // init locations
+    self.initLocations();
+
+    // init narrative strands
+    self.initStrands();
 
     self.title = scene.title;
     self.pageheadertitle =
@@ -52,6 +64,113 @@ function SceneTitleController($location, $routeParams, ChapterService,
 
     self.dirty = true;
 
+  }
+
+  self.initPointOfViews = function() {
+
+    // point of views
+    self.povs = [];
+
+    self.povs.push({
+      id: '1stOnMajor',
+      selected: true,
+      characterRelated: true
+    });
+    self.povs.push({
+      id: '1stOnMinor',
+      selected: true,
+      characterRelated: true
+    });
+    self.povs.push({
+      id: '3rdLimited',
+      selected: true,
+      characterRelated: true
+    });
+    self.povs.push({
+      id: '3rdOmniscient',
+      selected: true,
+      characterRelated: true
+    });
+    self.povs.push({
+      id: '3rdObjective',
+      selected: true,
+      characterRelated: true
+    });
+    self.povs.push({
+      id: '2nd',
+      selected: true,
+      characterRelated: true
+    });
+  }
+
+  self.initCharacters = function() {
+
+    // main characters
+    let mainCharacters = MainCharacterService.getMainCharacters();
+    self.characters = [];
+    for (let i = 0; i < mainCharacters.length; i++) {
+      self.characters.push({
+        id: mainCharacters[i].$loki,
+        name: mainCharacters[i].name,
+        main: true,
+        selected: true
+      });
+    }
+
+    // secondary characters
+    let secondaryCharacters = SecondaryCharacterService.getSecondaryCharacters();
+    for (let i = 0; i < secondaryCharacters.length; i++) {
+      self.characters.push({
+        id: secondaryCharacters[i].$loki,
+        name: secondaryCharacters[i].name,
+        main: false,
+        selected: true
+      });
+    }
+
+    // sort by name
+    self.characters.sort(function(a, b) {
+      return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
+    });
+  }
+
+  self.initLocations = function() {
+
+    // locations
+    let locations = LocationService.getLocations();
+    self.locations = [];
+    for (let i = 0; i < locations.length; i++) {
+      let name = LocationService.calculateLocationName(locations[i]);
+      self.locations.push({
+        id: locations[i].$loki,
+        name: name,
+        selected: true
+      });
+    }
+
+    // sort by name
+    self.locations.sort(function(a, b) {
+      return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
+    });
+  }
+
+  self.initStrands = function() {
+
+    // strands
+    let strands = StrandService.getStrands();
+    self.strands = [];
+    for (let i = 0; i < strands.length; i++) {
+      self.strands.push({
+        id: strands[i].$loki,
+        name: strands[i].name,
+        selected: true
+      });
+    }
+
+    // sort by name
+    self.strands.sort(function(a, b) {
+      return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
+    });
   }
 
   self.save = function() {
