6387b5bf068ecac2fb384f569e0c9a405eea8fff
Andrea Feccomandi <andreafeccomandi@gmail.com>
2017-11-25 23:30:28 +0100

    Introduced interview-switch

---
+++ bibisco/app/components/characters/interview-switch.js
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2014-2017 Andrea Feccomandi
+ *
+ * Licensed under the terms of GNU GPL License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ * See the GNU General Public License for more details.
+ *
+ */
+angular.
+  module('bibiscoApp').
+  component('interviewswitch', {
+    templateUrl: 'components/characters/interview-switch.html',
+    controller: InterviewSwitchController,
+    bindings: {
+      interviewactive: '='
+    }
+  });
+
+
+function InterviewSwitchController() {
+
+  var self = this;
+
+  self.$onInit = function () {
+    self.interviewactive = true;
+  };
+
+  self.interview = function() {
+    self.interviewactive = true;
+  };
+
+  self.freetext = function() {
+    self.interviewactive = false;
+  };
+}
