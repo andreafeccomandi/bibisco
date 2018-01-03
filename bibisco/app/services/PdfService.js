1ab647c2842493648f539038b7d3da7b635eab9c
Andrea Feccomandi <andreafeccomandi@gmail.com>
2018-01-03 01:43:49 +0100

    Integrated pdfmake

--- bibisco/app/services/PdfService.js
+++ bibisco/app/services/PdfService.js
@@ -18,7 +18,51 @@ angular.module('bibiscoApp').service('PdfService', function () {
 
   return {
     createFirstPdf: function() {
-      alert('PdfService.createFirstPdf() proviamo a creare il file...');
+    
+      var docDefinition = {
+        content: [
+          // if you don't need styles, you can use a simple string to define a paragraph
+          'This is a standard paragraph, using default style',
+
+          // using a { text: '...' } object lets you set styling properties
+          { text: 'Wtórna pierwsza osoba: historia jest opowiedziana z perpektywy pierwszej osoby przez drugorzędnego bohatera, który nie jest protagonistą relacjonującym wydarzenia. Ten punkt widzenia musi być stosowany, tylko jeżeli główni bohaterowie nie są świadomi swoich działań i dlatego nie są w stanie poprawnie opowiedzieć swojej historiii. Narrator nie zna myśli głównych bohaterów i może zrelacjonować tylko te wydarzenia, których był świadkiem.', fontSize: 15, alignment: 'justify' },
+
+          // using a { text: '...' } object lets you set styling properties
+          { text: 'Вы уверены, что хотите удалить эту сцену?', fontSize: 15, alignment: 'right' },
+
+          // if you set pass an array instead of a string, you'll be able
+          // to style any fragment individually
+          {
+            text: [
+              'This paragraph is defined as an array of elements to make it possible to ',
+              { text: 'restyle part of it and make it bigger ', fontSize: 15, bold: true},
+              { text: 'restyle part of it and make it bigger ', fontSize: 15, italics: true },
+              { text: 'restyle part of it and make it bigger ', fontSize: 15, decoration: 'underline', },
+              { text: 'restyle part of it and make it bigger ', fontSize: 15, bold: true, italics: true, decoration: 'underline' },
+              { text: 'Underline decoration', decoration: 'underline' },
+              { text: 'Underline as property', underline: true },
+              { text: 'Line Through decoration', decoration: 'lineThrough' },
+              { text: 'Overline decoration', decoration: 'overline' },
+              'than the rest.'
+            ], pageBreak: 'before', margin: [40, 40, 40, 40] 
+          }
+        ],
+        defaultStyle: {
+          font: 'courier'
+        }
+      };
+
+      pdfMake.fonts = {
+        courier: {
+          normal: 'cour.ttf',
+          bold: 'courbd.ttf',
+          italics: 'couri.ttf',
+          bolditalics: 'courbi.ttf'
+        }
+      };
+      
+      // download the PDF
+      pdfMake.createPdf(docDefinition).download('optionalName.pdf');    
     }
   };
 });
