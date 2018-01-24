a71c810bedebd04b86b7c10fb2e1559b02357cfd
Andrea Feccomandi <andreafeccomandi@gmail.com>
2018-01-24 08:43:53 +0100

    Bug fix: show main character info free text on project explorer

--- bibisco/app/components/project-explorer/project-explorer-maincharacter-info.js
+++ bibisco/app/components/project-explorer/project-explorer-maincharacter-info.js
@@ -23,10 +23,4 @@ angular.
     }
   });
 
-function ProjectExplorerMainCharacterInfoController() {
-
-  var self = this;
-
-  self.$onInit = function () {
-  };
-}
+function ProjectExplorerMainCharacterInfoController() {}
