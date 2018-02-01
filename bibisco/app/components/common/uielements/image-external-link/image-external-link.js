8bedb8cb460213000d7551a73a9e9ffdc692ba9f
Andrea Feccomandi <andreafeccomandi@gmail.com>
2018-02-02 00:10:28 +0100

    Added socialMediaTip tip

---
+++ bibisco/app/components/common/uielements/image-external-link/image-external-link.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2014-2017 Andrea Feccomandi
+ *
+ * Licensed under the terms of GNU GPL License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ * See the GNU General Public License for more details.
+ *
+ */
+angular.
+  module('bibiscoApp').
+  component('imageexternallink', {
+    templateUrl: 'components/common/uielements/image-external-link/image-external-link.html',
+    controller: ImageExternalLinkController,
+    bindings: {
+      src: '@',
+      url: '@'
+    },
+  });
+
+function ImageExternalLinkController() {
+
+  var self = this;
+  const { shell } = require('electron');
+
+  self.openUrl = function () {
+    shell.openExternal(self.url);
+  };
+}
